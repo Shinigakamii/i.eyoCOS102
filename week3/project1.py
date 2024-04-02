6affcf354d665e38cdef88e65247e136e1baa511
i.eyoCOS101 <shinigakamii@gmail.com>
2024-04-02 16:04:54 +0100

    new commit

--- week3/project1.py
+++ week3/project1.py
@@ -1,14 +1,33 @@
-#Importing the 'pandas' library as it is efficient in tabulating data
+data = [
+    {'Name': 'Evelyn', 'Gender': 'Female', 'Age': 17,  'Height' : 5.5, 'Scores' :83},
+    {'Name': 'Jessica', 'Gender': 'Female', 'Age': 16,  'Height' : 6.0, 'Scores' :85},
+    {'Name': 'Somto', 'Gender': 'Female', 'Age': 17,  'Height' : 5.4, 'Scores' :70},
+    {'Name': 'Edith', 'Gender': 'Female', 'Age': 18,  'Height' : 5.9, 'Scores' :60},
+    {'Name': 'Lizo', 'Gender': 'Female', 'Age': 16,  'Height' : 5.8, 'Scores' :76},
+    {'Name': 'Madonna', 'Gender': 'Female', 'Age': 18,  'Height' : 5.5, 'Scores' :66},
+    {'Name': 'Waje', 'Gender': 'Female', 'Age': 17,  'Height' : 6.1, 'Scores' :87},
+    {'Name': 'Tola', 'Gender': 'Female', 'Age': 20,  'Height' : 6.0, 'Scores' :95},
+    {'Name': 'Aisha', 'Gender': 'Female', 'Age': 19,  'Height' : 5.7, 'Scores' :60},
+    {'Name': 'Latifa', 'Gender': 'Female', 'Age': 17,  'Height' : 5.5, 'Scores' :49},
+    {'Name': 'Chinedu', 'Gender': 'Male', 'Age': 19,  'Height' : 5.7, 'Scores' :74},
+    {'Name': 'Liam', 'Gender': 'Male', 'Age': 16,  'Height' : 5.8, 'Scores' :87},
+    {'Name': 'Wale', 'Gender': 'Male', 'Age': 18,  'Height' : 5.8, 'Scores' :76},
+    {'Name': 'Gbenga', 'Gender': 'Male', 'Age': 17,  'Height' : 6.1, 'Scores' :68},
+    {'Name': 'Abiola', 'Gender': 'Male', 'Age': 20,  'Height' : 5.9, 'Scores' :66},
+    {'Name': 'Kola', 'Gender': 'Male', 'Age': 19,  'Height' : 5.5, 'Scores' :78},
+    {'Name': 'Kunle', 'Gender': 'Male', 'Age': 19,  'Height' : 6.1, 'Scores' :87},
+    {'Name': 'George', 'Gender': 'Male', 'Age': 18,  'Height' : 5.4, 'Scores' :98},
+    {'Name': 'Thomas', 'Gender': 'Male', 'Age': 17,  'Height' : 5.8, 'Scores' :54},
+    {'Name': 'Wesley', 'Gender': 'Male', 'Age': 19,  'Height' : 5.7, 'Scores' :60},
+    
+]
 
-import pandas as pd
-data = {
-    'Name' : ['Evelyn', 'Jessica', 'Somto', 'Edith', 'Lizo', 'Madonna', 'Waje', 'Tola', 'Aisha', 'Latifa', 'Chinedu', 'Liam', 'Wale', 'Gbenga', 'Abiola', 'Kola', 'Kunle', 'George', 'Thomas', 'Wesley'],
-    'Gender' : ['Female', 'Female', 'Female', 'Female', 'Female', 'Female', 'Female', 'Female', 'Female', 'Female', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male', 'Male'],
-    'Age' : [17, 16, 17, 18, 16, 18, 17, 20, 19, 17, 19, 16, 18, 17, 20, 19, 16, 18, 17, 19],
-    'Height' : [5.5, 6.0, 5.4, 5.9, 5.8, 5.5, 6.1, 6.0, 5.7, 5.5, 5.7, 5.8, 5.8, 6.1, 5.9, 5.5, 6.1, 5.4, 5.4, 5.8, 5.7],
-    'Scores' : [83, 85, 70, 60, 76, 66, 87, 95, 60, 49, 74, 87, 76, 68, 66, 78, 87, 98, 54, 60],
-} 
+# Extracting column names
+columns = list(data[0].keys())
 
-df = pd.DataFrame(data)
+# Printing column names
+print(" | ".join(columns))
 
-print(df)
+# Printing data rows
+for row in data:
+    print(" | ".join(str(row[column]) for column in columns))
